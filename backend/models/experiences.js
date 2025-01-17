1c7a4c95c78943eb60b1e56356203351ffd3626c
Lucy Kwak <bkwak17@gmail.com>
2025-01-16 19:44:25 -0500

    Update experienceSchema to focus on the experience only

--- backend/models/experiences.js
+++ backend/models/experiences.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
+const experienceSchema = mongoose.Schema({
    name: { type: String, required: true, unique: true },
-   // overallRating: 
-   // description: 
-   // delete rating and review 
-   // keep reviewedBy : users =---> use ref id insteaad 
-   
+   category: { type: String, required: true },
    overallRating: { type: Number }, // To be calculated and stored
-   review: { type: String, required: false },
-   reviewedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reviews' }],
 })
 
 module.exports = mongoose.model('Experience', experienceSchema);
