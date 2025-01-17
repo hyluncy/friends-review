b34805518f2b8f47693631e863da7f1b8eb41aad
Lucy Kwak <bkwak17@gmail.com>
2025-01-16 20:05:11 -0500

    Set up experiences route

---
+++ backend/routes/experience.js
@@ -0,0 +1,9 @@
+const express = require('express'); 
+const router = express.Router(); 
+const {
+    addExperience,
+    retrieveExperience,  
+} = require('../controllers/experience'); 
+
+ 
+module.exports = router; 
