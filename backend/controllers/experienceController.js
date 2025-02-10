bb63082f83c3accb609fd5a5d465693c0d48ec7b
Lucy Kwak <bkwak17@gmail.com>
2025-02-09 22:28:19 -0500

    Add in controllers and routes

--- backend/controllers/experienceController.js
+++ backend/controllers/experienceController.js
@@ -1,8 +1,14 @@
+const { findOne } = require("../models/user");
 
 
-const addExperience = (req, res) => {
+const addExperience = async (req, res) => {
     const [ name, category ] = req.body; 
-}
+    const existingExperience = await findOne(name); 
+
+    if (existingExperience) {
+        return res.status(400).json({ message: 'Experience already exists. Please refer to '
+    }
+}   
 
 const retrieveExperience = (req, res) => {
     
