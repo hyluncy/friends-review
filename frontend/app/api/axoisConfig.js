40b9c3bff864b73b62083ab3aeba922e5d1d6398
Lucy Kwak <bkwak17@gmail.com>
2025-02-19 22:45:29 -0500

    Fix localhost port issue

--- frontend/app/api/axoisConfig.js
+++ frontend/app/api/axoisConfig.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:4000',
     timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json'
+    }
   });
 
   export default axiosInstance;
