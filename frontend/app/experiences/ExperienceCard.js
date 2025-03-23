36098c2336fe0b6bd6ebaff46938b7f1d7e016d3
Lucy Kwak <bkwak17@gmail.com>
2025-03-22 21:43:10 -0400

    Create Experience Card to be shown on list

---
+++ frontend/app/experiences/ExperienceCard.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'; 
+import { Card } from 'react-bootstrap'; 
+
+export default function ExperienceCard ({ name, category }) {
+    return (
+        <Link href={`/experiences/${id}`}>
+            
+        </Link>
+    )
+}
