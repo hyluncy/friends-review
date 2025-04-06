cf704d55f587e8d7b0f103d0ef68218880387fcd
hyluncy <bkwak17@gmail.com>
2025-04-06 11:29:34 -0400

    Create an addExperience() to be used for addNew modal

--- frontend/app/api/axiosExperienceConfig.js
+++ frontend/app/api/axiosExperienceConfig.js
@@ -1,7 +1,20 @@
 import axios from 'axios'; 
+import axiosInstance from './axoisConfig';
 
 const axiosNext = axios.create({
     baseURL: 'http://localhost:4000/api', 
 }); 
+
+const addNewExperience = async() => {
+    try { 
+        const res = await axiosInstance.post('/api/experience/addNew', {
+            image: 'https://dummyimage.com/400x300/9c90e8/f7f7f7.png&text=No+Image+Available',
+            name: 'testName',
+            category: 'testCategory',
+        })
+    } catch (err) {
+        console.log('New Experience (Frontend) Failed - axiosExpConfig')
+    }
+}
 
 export default axiosNext;
