84f52b6ea02c0c053ee811ffe76d4aeec9f6137a
hyluncy <bkwak17@gmail.com>
2025-04-07 15:02:00 -0400

    Change button variant

--- frontend/app/experiences/ExperienceCard.js
+++ frontend/app/experiences/ExperienceCard.js
@@ -7,13 +7,13 @@ export default function ExperienceCard ({ exp }) {
     return (
         <Card>
             <Card.Img
-            variant="top"
-            src={exp.image || 'https://dummyimage.com/400x300/9c90e8/f7f7f7.png&text=No+Image+Available'}
-            alt={exp.name}
+                variant="top"
+                src={exp.image || 'https://dummyimage.com/400x300/9c90e8/f7f7f7.png&text=No+Image+Available'}
+                alt={exp.name}
             />
             <Card.Body>
                 <Card.Title>{exp.name}</Card.Title>
-                <Button variant="primary" onClick={() => router.push(`/experiences/${exp._id}`)}>
+                <Button variant="secondary" onClick={() => router.push(`/experiences/${exp._id}`)}>
                     View Experience
                 </Button>
             </Card.Body>
