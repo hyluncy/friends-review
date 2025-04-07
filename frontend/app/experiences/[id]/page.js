22a172821b4770dc5294c7d5a228337842b56154
hyluncy <bkwak17@gmail.com>
2025-04-07 15:00:41 -0400

    Create dynamic page for each experience

--- frontend/app/experiences/[id]/page.js
+++ frontend/app/experiences/[id]/page.js
@@ -1,12 +1,40 @@
-import { useRouter } from 'next/router'; 
-import Container from 'react-bootstrap'; 
+'use client'
+import { useRouter, useParams } from 'next/navigation'; 
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap'; 
+import { fetchExperienceById }  from '../../api/axiosExperienceConfig'; 
 
 export default function ExperienceIDPage() {
     const router = useRouter(); 
-    const { experienceID } = router.query; 
+    const params = useParams(); 
+    const id = params.id; 
+    const [ exp, setExp ] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async (id) => {
+            try {
+                const data = await fetchExperienceById(id)
+                console.log(data)
+                setExp(data)
+            } catch (err) {
+                console.log('Error fetchData [id]/page.js')
+                throw err
+            }
+        }
+        fetchData(id); 
+      }, [id]);
+
     return (
         <Container>
-            <h1>Experience Page</h1>
+            {exp && 
+                <>
+                    <h1>{exp.name}</h1>
+                    <br/>
+                    <img src={exp.image} alt='expImg'/>
+                    <br/><br/>
+                    <p><strong>Category:</strong> {exp.category}</p>
+                </>
+            }
         </Container>
     )
 }
