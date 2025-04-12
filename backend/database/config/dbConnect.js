684df09db6b0f4ae99544a0e2758cd4c12057072
hyluncy <bkwak17@gmail.com>
2025-04-12 10:15:32 -0400

    Add logger module for console.log and error statements

--- backend/database/config/dbConnect.js
+++ backend/database/config/dbConnect.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const logger = require('../../utils/logger'); 
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectToDB = async () => {
     try {
         await mongoose.connect(MONGO_URI); 
-        console.log('Connected to the database'); 
+        logger.info('Connected to the database'); 
     } catch (err) {
-        console.error('Error connecting to the database:', err.message); 
+        logger.error('Error connecting to the database:', err.message); 
     }
 }; 
 
